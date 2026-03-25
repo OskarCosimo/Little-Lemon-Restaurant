3df1b1aa0a205ad444348dcfa89a1406ab44eb03
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-03-25 15:56:35 +0100

    Delete components directory

--- components/AboutPage.jsx
+++
@@ -1,7 +0,0 @@
-export function AboutPage() {
-  return(
-    <main>
-      <></>
-    </main>
-  )
-}
--- components/OrdersPage.jsx
+++
@@ -1,7 +0,0 @@
-export function OrdersPage() {
-  return(
-    <main>
-      <></>
-    </main>
-  )
-}
